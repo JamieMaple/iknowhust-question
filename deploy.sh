a27e03146cef10a92b55a6461d48d24677431f6f
JamieMaple <jamiemaple007@gmail.com>
2018-05-21 14:45:40 +0800

    feat(new): for dong

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
+# dong
+cd ../iknow-question-build
+git checkout dong
+
+# back
+cd ../iknowhust-question
 npm run build
 cp -r build/* ../iknow-question-build
 
 cd ../iknow-question-build
 git add .
 git commit -m 'deploy'
-git push origin master
+git push origin dong
